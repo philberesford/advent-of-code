1bdf3e4e696999ae4b989bd846030f536c213cce
Phil <philberesford@users.noreply.github.com>
2020-12-04 11:55:48 +0000

    Refactored to allow easier parsing of values in validation routines.

--- 2020/day4.py
+++ 2020/day4.py
@@ -7,34 +7,40 @@ def main():
 
 def passport_counter(input, print_rows=False):    
     start_new_passport = True
-    passport_fields = []
+    passport = ""
     valid_count = 0
     row_count = 0
     for row in input:
         if row_count > 0 and row == "":              # An empty row starts a new passport
             start_new_passport = True 
-            if is_valid_passport(passport_fields): 
+            if is_valid_passport(passport): 
                 valid_count += 1    
         else:
             start_new_passport = False   
                 
         if start_new_passport:
-            passport_fields = [] 
-        
-        key_values = list(str(row).split())
-        keys = list(map(lambda key_value: key_value.split(":")[0], key_values))
-        for key in keys:
-            passport_fields.append(key)
+            passport = row
+        else:
+            passport += "\n" + row
 
         row_count += 1 
 
-    if is_valid_passport(passport_fields):  # Count the last one!
+    if is_valid_passport(passport):  # Count the last one!
         valid_count += 1 
 
     return valid_count
 
 
-def is_valid_passport(fields):
+def is_valid_passport(passport):
+    rows = passport.split("\n")
+
+    fields = []
+    for row in rows:
+        key_values = list(str(row).split())
+        keys = list(map(lambda key_value: key_value.split(":")[0], key_values))    
+        for key in keys:
+            fields.append(key)
+
     mandatory_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
     optional_fields = ["cid"]
 
