61d339a4f33049f6041a99c338b19f4d9a08e85c
Phil <philberesford@users.noreply.github.com>
2020-12-08 14:24:35 +0000

    Day 8, part 1 refactoring.

--- 2020/day8.py
+++ 2020/day8.py
@@ -3,14 +3,11 @@ class State:
         self.accumulator = accumulator
         self.instruction_index = instruction_index
 
-    def operate_on(self, instruction):
+    def apply_instruction(self, instruction):
         op, qty = instruction.split(" ")[:2] 
-        if op == "nop": 
-            return self.nop(int(qty))
-        if op == "acc": 
-            return self.acc(int(qty))
-        if op == "jmp": 
-            return self.jmp(int(qty))
+        if op == "nop": return self.nop(int(qty))
+        if op == "acc": return self.acc(int(qty))
+        if op == "jmp": return self.jmp(int(qty))
 
     def nop(self, num):
         return self.jmp(1)
@@ -29,23 +26,20 @@ def main():
     print("Accumulator before infinite loop: {}".format(state.accumulator))
 
 def bootSequence(instructions): 
-    state = State(0, 0)
-    infinite_loop_encountered = False
+    state = State(0, 0)    
     all_states = []
+    infinite_loop_encountered = False
     while not infinite_loop_encountered and state.instruction_index < len(instructions):
         processed = get_instruction_indexes(all_states)
-        if processed.count(state.instruction_index) > 1:
-            infinite_loop_encountered = True
-        else:
+        infinite_loop_encountered = processed.count(state.instruction_index) > 1
+        if not infinite_loop_encountered:
             instruction = instructions[state.instruction_index]
-            new_state = state.operate_on(instruction)
-            state = new_state
+            state = state.apply_instruction(instruction)
             all_states.append(state)
     
     return all_states[-1]   # Return the last state before we encountered the infinite loop
     
 def get_instruction_indexes(states):
     return list(map(lambda state: state.instruction_index, states))
-
 
 main()
