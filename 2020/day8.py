6836c2ae5bdd56dd2133d612d6636b599fb0356c
Phil <philberesford@users.noreply.github.com>
2020-12-08 14:28:28 +0000

    Added in private function definitions and renamed a few things to be clearer of intent.

--- 2020/day8.py
+++ 2020/day8.py
@@ -1,22 +1,22 @@
 class State:
-    def __init__(self, accumulator = 0, instruction_index = 0):
+    def __init__(self, accumulator = 0, instruction_pointer = 0):
         self.accumulator = accumulator
-        self.instruction_index = instruction_index
+        self.instruction_pointer = instruction_pointer
 
     def apply_instruction(self, instruction):
         op, qty = instruction.split(" ")[:2] 
-        if op == "nop": return self.nop(int(qty))
-        if op == "acc": return self.acc(int(qty))
-        if op == "jmp": return self.jmp(int(qty))
+        if op == "nop": return self.__nop(int(qty))
+        if op == "acc": return self.__acc(int(qty))
+        if op == "jmp": return self.__jmp(int(qty))
 
-    def nop(self, num):
-        return self.jmp(1)
+    def __nop(self, num):
+        return self.__jmp(1)
 
-    def acc(self, num):  
-        return State(self.accumulator + num, self.instruction_index + 1)
+    def __acc(self, num):  
+        return State(self.accumulator + num, self.instruction_pointer + 1)
 
-    def jmp(self, num):
-        return State(self.accumulator, self.instruction_index + num)
+    def __jmp(self, num):
+        return State(self.accumulator, self.instruction_pointer + num)
 
 def main():
     with open("day8.data",'r') as fs:
@@ -29,17 +29,17 @@ def bootSequence(instructions):
     state = State(0, 0)    
     all_states = []
     infinite_loop_encountered = False
-    while not infinite_loop_encountered and state.instruction_index < len(instructions):
-        processed = get_instruction_indexes(all_states)
-        infinite_loop_encountered = processed.count(state.instruction_index) > 1
+    while not infinite_loop_encountered and state.instruction_pointer < len(instructions):
+        processed = get_instruction_pointers(all_states)
+        infinite_loop_encountered = processed.count(state.instruction_pointer) > 1
         if not infinite_loop_encountered:
-            instruction = instructions[state.instruction_index]
+            instruction = instructions[state.instruction_pointer]
             state = state.apply_instruction(instruction)
             all_states.append(state)
     
     return all_states[-1]   # Return the last state before we encountered the infinite loop
     
-def get_instruction_indexes(states):
-    return list(map(lambda state: state.instruction_index, states))
+def get_instruction_pointers(states):
+    return list(map(lambda state: state.instruction_pointer, states))
 
 main()
