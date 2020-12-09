d691a28ffe8d134a450efac0d4e9d40b704deefd
Phil <philberesford@users.noreply.github.com>
2020-12-09 14:03:39 +0000

    Refactoring.

--- 2020/day9.py
+++ 2020/day9.py
@@ -2,25 +2,24 @@ def main():
     with open("day9.data",'r') as fs:
         input = list(map(lambda line: line.strip(), fs.readlines()))
 
-    part1 = get_number_that_is_not_in_preceding_pair_sums(25, input)
+    numbers = list(map(int, input))
+    part1 = get_number_that_is_not_in_preceding_pair_sums(25, numbers)
     print("The invalid number is: {}".format(part1))
 
-    part2 = get_preceding_set_of_numbers_that_sum(part1, input)
+    part2 = get_preceding_set_of_numbers_that_sum(part1, numbers)
     print("Encryption weakness: {}".format(min(part2) + max(part2)))
 
-def get_preceding_set_of_numbers_that_sum(target, input):
-    numbers = list(map(int, input))
+def get_preceding_set_of_numbers_that_sum(target, numbers):
     for i in range(0, len(numbers)):
-        for j in range(i-1, 0, -1):  
-            to_sum = numbers[j:i-1]    
-            total = sum(to_sum)
+        for j in range(i-1, 0, -1):  # Get all indexes in the range starting at the current number and work backwards
+            candidate_numbers = numbers[j:i-1]    
+            total = sum(candidate_numbers)
             if total == target:
-                return to_sum
+                return candidate_numbers
     return None     # On no! None found.
 
-def get_number_that_is_not_in_preceding_pair_sums(preamble_size, input):
-    numbers = list(map(int, input))
-    for i in range(preamble_size, len(input)):
+def get_number_that_is_not_in_preceding_pair_sums(preamble_size, numbers):
+    for i in range(preamble_size, len(numbers)):
         num = numbers[i]
         preamble = numbers[i-preamble_size:i]           # Get the preceeding n numbers 
         if not num in create_sum_of_pairs(preamble):
@@ -35,13 +34,5 @@ def create_sum_of_pairs(set):
     
     pair_sums = list(map(sum, pairs))
     return pair_sums
-
-# def create_sets_of_n_numbers(size, input):
-#     numbers = list(map(int, input))
-#     result = []
-#     for i in range(0, len(input)-size+1):        
-#         set = numbers[i:i+size]
-#         result.append(set)
-#     return result
 
 main()
