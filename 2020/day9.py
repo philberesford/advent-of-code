1a29dc293d027dcd41282876eaceb0c3ef3db807
Phil <philberesford@users.noreply.github.com>
2020-12-09 13:59:35 +0000

    Day 9, part 2.

--- 2020/day9.py
+++ 2020/day9.py
@@ -3,7 +3,20 @@ def main():
         input = list(map(lambda line: line.strip(), fs.readlines()))
 
     part1 = get_number_that_is_not_in_preceding_pair_sums(25, input)
-    print("The number is: {}".format(part1))
+    print("The invalid number is: {}".format(part1))
+
+    part2 = get_preceding_set_of_numbers_that_sum(part1, input)
+    print("Encryption weakness: {}".format(min(part2) + max(part2)))
+
+def get_preceding_set_of_numbers_that_sum(target, input):
+    numbers = list(map(int, input))
+    for i in range(0, len(numbers)):
+        for j in range(i-1, 0, -1):  
+            to_sum = numbers[j:i-1]    
+            total = sum(to_sum)
+            if total == target:
+                return to_sum
+    return None     # On no! None found.
 
 def get_number_that_is_not_in_preceding_pair_sums(preamble_size, input):
     numbers = list(map(int, input))
