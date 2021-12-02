9ca691e69ffa8cd34d0f2be37b429d72faf9db18
Phil <philberesford@users.noreply.github.com>
2021-12-02 12:54:07 +0000

    Day 2.

--- 2021/day2.py
+++ 2021/day2.py
@@ -5,17 +5,20 @@ def app():
         input = read_input("day2.data")
         horizontal = 0
         depth = 0
+        aim = 0
         for cmd in input:
             amount = get_amount(cmd)
             if cmd.startswith("forward"):
+                depth += (aim * amount)
                 horizontal += amount
             if cmd.startswith("up"):
-                depth -= amount
+                aim -= amount
+                # depth -= amount
             if cmd.startswith("down"):
-                depth += amount
-        part1 = horizontal * depth
-
-        print(part1)
+                aim += amount
+                # depth += amount
+        part2 = horizontal * depth
+        print(part2)
 
 def get_amount(command: str) -> int:
     return int(command.split(" ")[1])
