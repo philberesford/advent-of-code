91770353679c962fc0d26171818a1e28219db261
Phil <philberesford@users.noreply.github.com>
2023-12-05 08:53:59 +0000

    Getting the basic file reader stuff set up.

--- 2023/day1.ts
+++ 2023/day1.ts
@@ -1,1 +1,12 @@
-console.log("Hello")
+import { readAsStrings, workingDirectory } from "./io";
+import * as path from 'path';
+
+
+const main = async () => {
+    const workingPath = workingDirectory();
+    const filePath = path.join(workingPath, "day1.data");
+    const strings = await readAsStrings(filePath);
+    console.log(strings);
+}
+
+(async function(){ await main() })()
---
+++ 2023/io.ts
@@ -0,0 +1,10 @@
+import * as fs from "fs";
+
+export const readAsStrings = async (path: string): Promise<string[]> => {
+    const content= await fs.readFileSync(path, "utf8");
+    return content.split("\n").map(s => s.trim());
+}
+
+export const workingDirectory = (): string => {
+    return __dirname;
+}
